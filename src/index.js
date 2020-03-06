db28b1ae69f31dc9aaa78726cdbea006f5bac8d5
Victor Markov <victor.markov@urbanise.com>
2020-03-06 20:24:30 +0200

    AAAA

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import Phaser from "phaser";
 import * as _ from 'lodash';
-import { FirstScene } from "./scenes/FirstScene";
+import { TitleScene } from "./scenes/TitleScene";
+import { OptionsScene } from "./scenes/OptionsScene";
+import { MainScene } from "./scenes/MainScene";
 
 export const config = {
   type: Phaser.AUTO,
   parent: "phaser-example",
   width: 800,
   height: 600,
-  scene: [FirstScene]
+  scene: [TitleScene, MainScene, OptionsScene]
 };
 
 const game = new Phaser.Game(config);
